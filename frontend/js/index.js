52692dfd65d05d3725bfe8e00563f632b2b9ff65
user <irin-shkuratov8@yandex.ru>
2025-02-11 15:05:02 +0300

    /

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,15 +1,15 @@
-const btn1 = document.getElementById("btn1");
-const btn2 = document.getElementById("btn2");
+const btn1 = document.getElementById("btn1"); // получаем кнопку по id
+const btn2 = document.getElementById("btn2"); // получаем другую кнопку по id
 
-const p = document.querySelector(".p");
-const error = document.querySelector(".error");
-p.className += " display_none";
+const p = document.querySelector(".p"); // получаем текст по названию классса из main.css
+const error = document.querySelector(".error"); // получаем текст по названию классса из main.css
+p.className += " display_none"; // добавляем к елементу класс со стилями из main.css
 
-let count1 = 0;
-let flag = false;
-let res = null;
+let count1 = 0; // инициализируем счётчик для подсчёта количества нажатий.
+let flag = false; // значение: можно ли запускать процедуру
+let res = null; // сюда записываем значение выбранной процедуры
 
-elements = [
+elements = [ // список с названиями процедур
     "name1",
     "name2",
     "name3",
@@ -20,62 +20,62 @@ elements = [
     "name8",
 ];
 
-function getRadioValue() {
-    var radio = document.getElementsByName("name");
-    for (i = 0; i < radio.length; i++) {
-        if (radio[i].checked) {
-            flag = true;
-            const div = document.querySelector(".menu");
-            res = radio[i].value;
-            div.remove();
+function getRadioValue() { // функция для получения значения при выборе процедуры
+    var radio = document.getElementsByName("name");// получаем круглые кнопки по названию имени
+    for (i = 0; i < radio.length; i++) { // проходимся по ним циклом
+        if (radio[i].checked) { // если кнопка выбрана
+            flag = true; // можно запускать процедуру
+            const div = document.querySelector(".menu"); // получаем форму с круглыми кнопками по названию класса из main.css
+            res = radio[i].value; // записываем название процедуры
+            div.remove(); // закрываем форму
             count1++;
-            p.className = "p";
-            error.className += " display_none";
+            p.className = "p"; // тексту меняем класс из main.css
+            error.className += " display_none"; // скрываем текст с ошибкой
         }
     }
 }
 
-btn1.addEventListener("click", () => {
+btn1.addEventListener("click", () => { // функция, которая срабатывает при нажатии на кнопку "Выбрать процедуру"
     if (count1 % 2 == 0) {
-        const div = document.createElement("div");
-        div.className = "menu";
-        for (let i = 0; i < elements.length; i++) {
-            const div2 = document.createElement("div");
-            const label = document.createElement("label");
-            const radio = document.createElement("input");
+        const div = document.createElement("div"); // создаём форму для круглых кнопок
+        div.className = "menu"; // подключаем класс из main.css
+        for (let i = 0; i < elements.length; i++) { // циклом идём по списку процедур
+            const div2 = document.createElement("div"); // создаём блок
+            const label = document.createElement("label"); // создаём текст
+            const radio = document.createElement("input"); // создаём круглую кнопку
 
-            radio.type = "radio";
-            radio.name = "name";
-            radio.value = elements[i];
-            radio.className = "radio";
-            label.innerText = elements[i];
-            label.className = "label";
-            div2.className = "block";
+            radio.type = "radio"; // кнопке даём тип
+            radio.name = "name"; // кнопке даём имя группы
+            radio.value = elements[i]; // кнопке присваиваем значение
+            radio.className = "radio"; // кнопке даём класс из main.css
+            label.innerText = elements[i]; // наполняем элемент текстом
+            label.className = "label"; // даём класс из main.css
+            div2.className = "block"; // даём класс из main.css
 
-            div2.appendChild(radio);
-            div2.appendChild(label);
-            div.appendChild(div2);
+            div2.appendChild(radio); // в блок помещаем круглую кнопку
+            div2.appendChild(label); // в блок помещаем текст
+            div.appendChild(div2); // в форму помещаем блок
         }
-        const btn3 = document.createElement("button");
-        btn3.className = "btn t3";
-        btn3.id = "btn3";
-        btn3.innerText = "Выбрать";
-        div.append(btn3);
-        document.querySelector(".main").appendChild(div);
-        btn3.addEventListener("click", getRadioValue);
+        const btn3 = document.createElement("button"); // создаём кнопку
+        btn3.className = "btn t3"; // даём ей класс из main.css
+        btn3.id = "btn3"; // даём ей id
+        btn3.innerText = "Выбрать"; // устанавливаем ей текст
+        div.append(btn3); // в форму добавляем кнопку
+        document.querySelector(".main").appendChild(div); // форму добавляем на страницу
+        btn3.addEventListener("click", getRadioValue); // при нажатии на кнопку вызываем функцию getRadioValue
     } else {
-        const div = document.querySelector(".menu");
-        div.remove();
+        const div = document.querySelector(".menu"); // получаем форму по классу
+        div.remove(); // удаляем форму
     }
     count1++;
     
 });
 
-btn2.addEventListener("click", () => {
-    if (!flag) {
-        error.className = "error";
+btn2.addEventListener("click", () => { // функция, которая срабатывает при нажатии на кнопку "Приступить"
+    if (!flag) { // знак ! эквивалентен not в python
+        error.className = "error"; // елементу устанавливаем класс
     } else {
-        error.className += " display_none";
-        eel.hello(res);
+        error.className += " display_none"; // елементу добавляем класс
+        eel.hello(res); // вызываем функцию hello в python
     }
 });
--- main.py
+++ main.py
@@ -1,32 +1,35 @@
-import eel
-from pygame import mixer
+import eel # подключаем библиотеку eel
+from pygame import mixer # из библиотеки pygame подкльчаем модуль mixer
 
-count = 0
+count = 0 # инициализируем счётчик для подсчёта количества нажатий.
 
-def close_callback(route, websockets):
-    if not websockets:
-        exit()
+def close_callback(route, websockets): # функция для остановки приложения
+    if not websockets: # если нажат крестик
+        exit() # остановка программы
 
 
-@eel.expose
-def hello(res):
-    global count
+@eel.expose # декаратор для того, чтобы функцию можно было вызывать из javascript
+def hello(res): # res - название процедуры
+    global count # подключаем счётчик
     print("Hello, Sonya and Lera!")
     print(res)
-    mixer.init()
-    mixer.music.load("music/Uspokaivayushhaya_muzyka_-_lejjta_56569736.mp3")
+    mixer.init() # активируем модуль для проигрывания музыки
+    mixer.music.load("music/Uspokaivayushhaya_muzyka_-_lejjta_56569736.mp3") # загружаем трек
     if count % 2 == 0:
-        mixer.music.play()
+        mixer.music.play() # включаем трек
     else:
-        mixer.music.stop()
+        mixer.music.stop() # останавливаем трек
     count += 1
 
 def main():
-    eel.init("frontend")
-    eel.start(
+    eel.init("frontend") # подключаем папку с кодом приложения
+    eel.start( # активируем окно приложения
         "index.html", close_callback=close_callback, cmdline_args=["--start-maximized"]
     )
+    # index.html - файл с кодом приложения
+    # close_callback - подключение функции для звкрытия окна
+    # cmdline_args=["--start-maximized"] - открытие окна на весь экран
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__": # если запуск программы из файла main.py
+    main() # то вызываем функцию main и запускаем приложение
