e2480dbfb346e50c167fc38c0b888ec8f3c53caa
Visiliy <151849803+Visiliy@users.noreply.github.com>
2025-02-13 19:21:06 +0300

    /

--- main.py
+++ main.py
@@ -1,28 +1,60 @@
-import eel # подключаем библиотеку eel
+import eel
 import cv2
+import json
 import mediapipe
-import numpy as np
-from pygame import mixer # из библиотеки pygame подкльчаем модуль mixer
+from pygame import mixer
 
-count = 0 # инициализируем счётчик для подсчёта количества нажатий.
 
 def close_callback(route, websockets): # функция для остановки приложения
     if not websockets: # если нажат крестик
         exit() # остановка программы
 
 
-@eel.expose # декаратор для того, чтобы функцию можно было вызывать из javascript
+@eel.expose # декоратор для того, чтобы функцию можно было вызывать из javascript
 def hello(res): # res - название процедуры
-    global count # подключаем счётчик
-    print("Hello, Sonya and Lera!")
-    print(res)
     mixer.init() # активируем модуль для проигрывания музыки
     mixer.music.load("music/Uspokaivayushhaya_muzyka_-_lejjta_56569736.mp3") # загружаем трек
-    if count % 2 == 0:
-        mixer.music.play() # включаем трек
-    else:
-        mixer.music.stop() # останавливаем трек
-    count += 1
+    mixer.music.play() # включаем трек
+
+    mp_solutions = mediapipe.solutions.face_mesh
+    face = mp_solutions.FaceMesh(min_detection_confidence=0.5)
+
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        raise IOError("Ошибка видео")
+
+    with open(f"library/{res}.txt", "r", encoding="utf-8") as f:
+        pocket = json.loads(f.read())
+
+    try:
+        while True:
+            ret, frame = cap.read()
+            results = face.process(frame)
+            if results.multi_face_landmarks:
+                for face_land in results.multi_face_landmarks:
+                    idx = 0
+                    for elem in face_land.landmark:
+                        if idx in pocket:
+                            x = int(elem.x * frame.shape[1])
+                            y = int(elem.y * frame.shape[0])
+                            face_mark = [x, y]
+                            # if face_mark in coordinates:
+                            #     pocket.append(idx)
+                            cv2.circle(frame, (x, y), 3, (0, 225, 0), -1)
+                        idx = idx + 1
+
+            cv2.putText(frame, "", (120, 100), cv2.FORMATTER_FMT_NUMPY, 2, (0, 0, 0), 2)
+            frame = cv2.flip(frame, 1)
+            cv2.imshow("Video", frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                mixer.music.stop()
+                break
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
+
 
 def main():
     eel.init("frontend") # подключаем папку с кодом приложения
@@ -30,7 +62,7 @@ def main():
         "index.html", close_callback=close_callback, cmdline_args=["--start-maximized"]
     )
     # index.html - файл с кодом приложения
-    # close_callback - подключение функции для звкрытия окна
+    # close_callback - подключение функции для закрытия окна
     # cmdline_args=["--start-maximized"] - открытие окна на весь экран
 
 
